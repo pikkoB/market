411438031374499118cb74c233790f9ae17f6d7b
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2023-03-24 22:55:21 -0500

    variables

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,11 +1,12 @@
 const { Sequelize } = require("sequelize");
+require('dotenv').config()
 
 const db = new Sequelize({
   database: "marketplace",
   host: "localhost",
   port: 5432,
   username: "postgres",
-  password: "root2616",
+  password: process.env.DB_CONFIG_PASSWORD,
   dialect: "postgres",
   logging: false,
 });
